0ebbdc0707d4353c4dca342998cac8bc8da0738b
parth <parthbhadra14@gmail.com>
2023-11-19 22:27:38 +0530

    function overloading and encapsulation

--- oops/inheritance.js
+++ oops/inheritance.js
@@ -1,13 +1,32 @@
 class Parent {
+    // constructor overloading is not allowed
+    // because in js
+    // a class is a syntactic sugar over function
+    // constructor(nameP, age) {
+    //     this.nameP = nameP;
+    //     this.age = age; // encapsulation
+    // }
     constructor(nameP) {
         this.nameP = nameP;
     }
+
+    getName = () => {
+        return this.nameP;
+    }
+
+    getName = (n) => {
+        return n;
+    }
 }
 
-let p = new Parent("Father");
-console.log(p);
+// let p1 = new Parent("Father1", 30);
+// console.log(p1);
 
-class Child extends Parent {
+let p2 = new Parent("Father2");
+console.log(p2);
+p2.getName("hello");
+
+class Child extends Parent { // inheritance
     constructor(name) {
         super("Father");
         this.name = name;
@@ -15,6 +34,7 @@ class Child extends Parent {
 }
 
 let c = new Child("Son");
-
 console.log(c);
+
+// this was inheritance using class
 
