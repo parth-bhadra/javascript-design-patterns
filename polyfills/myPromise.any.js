c6534a05bbf2dc2247c92f33f163bdb6cd1cccf1
parth <parthbhadra14@gmail.com>
2023-11-29 12:40:43 +0530

    promise.any.js success case

--- polyfills/myPromise.any.js
+++ polyfills/myPromise.any.js
@@ -1,45 +1,42 @@
-async function handler() {
-  let a = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("inside api 1");
-      resolve("api 1 success");
-      //   reject("api 1 fail");
-    }, 3000);
+Promise.myAny = async function (promises) {
+  return new Promise(async (resolve, reject) => {
+    // promises.forEach(async promise => {
+    //   // everything inside of a for each is async task and is non blocking
+    //   resolve(await Promise.resolve(promise));
+    // });
+
+    // implementation with for loop
+    // async at the top is only for the for loop below
+    // for (let i = 0; i < promises.length; i++) {
+    //   // everything inside of a for loop is a sync task and is blocking
+    //   resolve(await Promise.resolve(promises[i]));
+    // }
+
+    // implementation with for loop
+    for (let i = 0; i < promises.length; i++) {
+      // everything inside of a for loop is a sync task and is blocking
+      // modifying the for loop to make it behave asynchronously
+      setTimeout(async () => {
+        resolve(await Promise.resolve(promises[i]));
+      }, 0);
+    }
   });
+};
 
-  let b = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("inside api 2");
-      resolve("api 2 success");
-      // reject("api 2 fail");
-    }, 5000);
-  });
+let a = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("inside api 1");
+    resolve("api 1 success");
+    //   reject("api 1 fail");
+  }, 3000);
+});
 
-  let c = 10;
+let b = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("inside api 2");
+    resolve("api 2 success");
+    // reject("api 2 fail");
+  }, 5000);
+});
 
-  // console.log(await Promise.any([a, b, c]));
-
-  Promise.myAny = function (promises) {
-    let arr = new Array();
-
-    return new Promise(async (resolve, reject) => {
-      try {
-        // for (let i = 0; i < promises.length; i++) {
-        //   arr.push(promises[i]);
-        // }
-        // struggling with the problem statement, it is realized that this method needs to execute promises on a different thread
-        promises.forEach(async (element) => {
-          arr.push(element);
-        });
-
-        resolve(arr[0]);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  };
-
-  console.log("Output ", await Promise.myAny([a, b, c]));
-}
-
-handler();
+console.log(await Promise.myAny([b, a, 10]));
